07336c3c38d90ed8e01d1971870857edae77a2d6
Kang-Yeonjung <yeonjungkang@dgu.ac.kr>
2023-10-08 16:08:14 +0900

    실습과제#6 5번

--- info.py
+++ info.py
@@ -1,6 +1,8 @@
 name = str(input("이름을 입력하세요: "))
 stuID = int(input("학번을 입력하세요: "))
 maj = str(input("학과를 입력하세요: "))
+grade = int(input("학년을 입력하세요: "))
 print("이름: " + name)
 print("학번:", stuID)
 print("학과: " + maj)
+print("학년:", grade)
